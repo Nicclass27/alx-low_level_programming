f334f13c2a04b0fa7b1415fd6251e3b61273b5f3
nicclass27 <nicclass27@gmail.com>
2022-03-24 12:56:09 -0700

    alx-low_level_programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,8 +8,10 @@
 {	
 	int n, m; 	for (n = 48; n <= 56; n++)	{		for (m = 49; m <= 57; m++)		{			if (m > n)			{				putchar(n);
 		putchar(m);				if (n != 56 || m != 57)
-		{					putchar(',');					putchar(' ');				}
-	}		}	}	putchar('\n');
+		{					putchar(',');					putchar(' ');				
+		}
+	}
+}	}	putchar('\n');
 
 	return (0);
 }
